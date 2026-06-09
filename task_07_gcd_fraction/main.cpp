840d3436103f28de90d9ad31c5d1ddd4725bca5a
Zembo271 <Zembjakmartinko@gmail.com>
2026-06-09 15:58:49 +0200

    DU

--- task_07_gcd_fraction/main.cpp
+++ task_07_gcd_fraction/main.cpp
@@ -1,15 +1,37 @@
 
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 long long gcdll(long long a, long long b) {
     // TODO: Euclid's algorithm (handle negatives)
     return 1;
 }
 int main() {
+    using namespace std;
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    long long p,q;
-    if (!(std::cin >> p >> q)) return 0;
+    long long p, q;
+    if (!(std::cin >> p >> q))
+        return 0;
+    cin >> p >> q;
+
+    long long a = llabs(p);
+    long long b = llabs(q);
+
+    while (b != 0) {
+        long long r = a % b;
+        a = b;
+        b = r;
+    }
+
+    p /= a;
+    q /= a;
+
+    if (q < 0) {
+        p = -p;
+        q = -q;
+    }
+
+    cout << p << " " << q;
     // TODO: reduce, ensure q'>0, and print p' q'
     return 0;
 }
--- task_10_binary_search_first/main.cpp
+++ task_10_binary_search_first/main.cpp
@@ -1,14 +1,35 @@
 
 #include <iostream>
 #include <vector>
+using namespace std;
 int main() {
+
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    int n; 
-    if (!(std::cin >> n)) return 0;
-    std::vector<long long> a(n);
-    for (int i=0;i<n;i++) std::cin >> a[i];
-    long long x; std::cin >> x;
-    // TODO: binary search for first index of x; print index or -1
+    int n;
+    if (!(std::cin >> n))
+        return 0;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+        std::cin >> a[i];
+    long long x;
+    cin >> x;
+    int l = 0, r = n - 1;
+    int ans = -1;
+
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+
+        if (a[m] == x) {
+            ans = m;
+            r = m - 1;
+        } else if (a[m] < x) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+
+    cout << ans;
     return 0;
 }
